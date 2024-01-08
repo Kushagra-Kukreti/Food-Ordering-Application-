2cef57165b1226752f9f4e14bbe460911bbf119a
Kushagra Kukreti <kukretikushagra@gmail.com>
2024-01-08 18:36:04 +0530

    Category filter added & icon changed

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,39 +1,19 @@
 import { useState } from "react";
 import StoreItem from "../components/StoreItem";
-import {StoreItemProp} from '../components/StoreItem'
 import storeItems from "../data/items.json";
 import Dropdown from "../components/Dropdown";
 
 const Store = () => {
   const [search, setSearch] = useState("");
-  const [val,setVal] =useState("")
-  const [title,setTitle] = useState("");
+  const [val, setVal] = useState("");
+  const [title, setTitle] = useState("");
 
-         const getSorted = (t:string,c:string)=>{
-             
-                setVal(c);
-                setTitle(t);
-    
-        }
+  const getSorted = (t: string, c: string) => {
+    setVal(c);
+    setTitle(t);
+  };
 
-        const sortData = (a:StoreItemProp,b:StoreItemProp)=>{
-          if(title === "Price"){
-            if(val === "High to Low")
-            return b.price-a.price
-            else if(val === "Low to High")
-            return a.price-b.price
-          return 0;
-          }
-          else if(title === "Rating"){
-            if(val === "High to Low")
-            return b.rating-a.rating
-            else if(val === "Low to High")
-            return a.rating-b.rating
-          return 0;
-          }
-          else return 0;
-            
-        }
+  
 
   return (
     <>
@@ -42,14 +22,25 @@ const Store = () => {
           display: "flex",
           flexDirection: "row",
           gap: "0.5rem",
-          marginBottom:"0.3rem",
-          marginTop:"0.3rem"
+          marginBottom: "0.3rem",
+          marginTop: "0.3rem",
         }}
       >
-        
-        <Dropdown getSorted = {getSorted} title={"Rating"} menuInfo ={["High to Low","Low to High"]} />
-        <Dropdown getSorted = {getSorted} title={"Price"} menuInfo ={["High to Low","Low to High"]} />
-        <Dropdown getSorted = {getSorted} title={"Category"} menuInfo ={["Car", "Book","Computer","Banana"]}/>
+        <Dropdown
+          getSorted={getSorted}
+          title={"Rating"}
+          menuInfo={["High to Low", "Low to High"]}
+        />
+        <Dropdown
+          getSorted={getSorted}
+          title={"Price"}
+          menuInfo={["High to Low", "Low to High"]}
+        />
+        <Dropdown
+          getSorted={getSorted}
+          title={"Category"}
+          menuInfo={["Car", "Book", "Computer", "Banana"]}
+        />
 
         <input
           type="text"
@@ -68,7 +59,24 @@ const Store = () => {
 
       <div className="row">
         {storeItems
-        .sort((a,b)=>sortData(a,b))
+          .filter((currItem) => {
+            if (title === "Category") {
+              if (val === currItem.category) {
+                return currItem;
+              }
+            } else return currItem;
+          })
+          .sort((a, b) =>{
+            if (title === "Price") {
+              if (val === "High to Low") return b.price - a.price;
+              else if (val === "Low to High") return a.price - b.price;
+              return 0;
+            } else if (title === "Rating") {
+              if (val === "High to Low") return b.rating - a.rating;
+              else if (val === "Low to High") return a.rating - b.rating;
+              return 0;
+            } else return 0;
+          })
           .filter((i) => {
             return search.toLowerCase() == " "
               ? i
